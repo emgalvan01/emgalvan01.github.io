9ee0ff0ef3a338dd2d0549c94db702c08d9294fa
Emmanuel Galvan <81732035+emgalvan01@users.noreply.github.com>
2021-04-26 22:56:44 -0600

    Uploaded Forward Fitness Bootstrap via upload

---
+++ ForwardFitnessBootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Emmanuel Galvan
+    File Name: script.js
+    Date: 4/26/2021
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
