b27f13135e1d1fb17463b4bdb357243d7a10a221
Emmanuel Galvan <81732035+emgalvan01@users.noreply.github.com>
2021-04-28 22:39:37 -0600

    Added Strike a Chord files via upload

---
+++ StrikeAChord/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:
+    File Name: script.js
+    Date: 
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
