063b024f98f759f65b5c00692a4ffeb37e2f5050
Emmanuel Galvan <81732035+emgalvan01@users.noreply.github.com>
2021-04-28 23:14:30 -0600

    Added Wild Rescues files via upload

---
+++ WildRescues/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Emmanuel Galvan
+    File Name: script.js
+    Date: 4/28/2021
+*/
+
+//jQery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
